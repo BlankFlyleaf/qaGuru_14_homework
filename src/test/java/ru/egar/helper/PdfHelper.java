c467230c3fac119f28569620603f8e4519a4534b
BlankFlyleaf <kriechenrosenrot@gmail.com>
2026-07-06 10:28:47 +0300

    Все таки пробую запустить PDF

--- src/test/java/ru/egar/helper/PdfHelper.java
+++ src/test/java/ru/egar/helper/PdfHelper.java
@@ -2,9 +2,6 @@ package ru.egar.helper;
 
 import com.codeborne.pdftest.PDF;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,8 +10,7 @@ public class PdfHelper {
 
     private PDF getPdf(String value) throws Exception {
         if (pdf == null) {
-            String encoded = URLEncoder.encode(value, StandardCharsets.UTF_8);
-            pdf = new PDF($("[href='https://file.egar.ru/" + encoded + ".pdf']").download());
+            pdf = new PDF($("a[href*='" + value + "']").download());
         }
         return pdf;
     }
